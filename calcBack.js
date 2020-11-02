79e6989bcab966c131dc321c86be99b79f6bc281
Hafis cp <cphafis3@gmail.com>
2020-11-03 00:39:59 +0530

    Done Some Changes( Removed blank spaces & unwanted, Added some comments ) - Calculator Project

--- calcBack.js
+++ calcBack.js
@@ -1,13 +1,17 @@
-function buttonClick(val){
-    console.log(val)
-    document.getElementById("display").value=document.getElementById("display").value+val 
+function buttonClick(val)
+{
+    document.getElementById("display").value=document.getElementById("display").value+val
+    //document.getElementById("display").value+=val
 }
 
-function clearDisplay(){
+function clearDisplay()
+{
     document.getElementById("display").value=" "
+    //Clear The Texts, Printing a white space
 }
 
-function sumResult(){
+function sumResult()
+{
     var text=document.getElementById("display").value
     var result=eval(text)
     document.getElementById("display").value=result
